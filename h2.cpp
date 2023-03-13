19a30ec54919eff78e1d6c8dccb07d6a1e2ac95f
Sahil Tambe <sahiltambe81922@gmail.com>
2023-03-13 19:10:47 +0530

    commited 2nd and 3rd

---
+++ h2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main( )
+  {
+cout<<"hiii bhaiii";
+return 0 ;
+}
---
+++ h3.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main( )
+  {
+cout<<"maa chudao";
+return 0 ;
+}
